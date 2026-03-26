5d0a82e0b96ff571959bbf1a1fff82d20772644f
Candy Designs <ianmiruka1@gmail.com>
2026-03-26 16:17:57 +0300

    Update Candy_Install.sh

--- Candy_Install.sh
+++ Candy_Install.sh
@@ -4732,6 +4732,7 @@ else
 fi
 EOF
 chmod +x "$HOME/.config/hypr/scripts/xray.sh"
+chmod +x "$HOME/.config/hyprcandy/scripts/wallpaper.sh"
 chmod +x "$HOME/.config/quickshell/wallpaper/wallpaper-apply.sh"
 chmod +x "$HOME/.config/quickshell/wallpaper/wallpaper-cycle.sh"
 
