0d6324f7f709270810c2940dc7fc7ad393683901
Candy Designs <ianmiruka1@gmail.com>
2026-04-21 01:32:56 +0300

    Update directory creation in Candy_Update.sh
    Removed creation of the mako config directory.

--- Candy_Update.sh
+++ Candy_Update.sh
@@ -1103,7 +1103,7 @@ echo "🗑️  Cleaned up temporary update directory"
 
 ### ✅ Setup mako config, hook scripts and needed services
 echo "📁 Creating background hook scripts..."
-mkdir -p "$HOME/.config/hyprcandy/hooks" "$HOME/.config/systemd/user" "$HOME/.config/mako" "$HOME/.config/pypr" 
+mkdir -p "$HOME/.config/hyprcandy/hooks" "$HOME/.config/systemd/user" "$HOME/.config/pypr" 
 
 ### 🪧 Setup Hyprland config
 cat > "$HOME/.config/hypr/hyprland.conf" << 'EOF'
